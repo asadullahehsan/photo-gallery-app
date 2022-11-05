0b59455ad293f0be22a46f6988434bbca7c9b58a
asadullahehsan <asadullahehsan10@gmail.com>
2022-11-05 22:34:04 +0500

    Add a new standalone component
    Create a new standalone component that allows the user to send feedback and chat with a virtual agent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
+import { FeedbackAndChatComponent } from './feedback-and-chat/feedback-and-chat.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FeedbackAndChatComponent],
 })
 export class AppComponent {
   title = 'photo-gallery-app';

01edb9a3756df39b9d965a50f7ac19416e535bab
asadullahehsan <asadullahehsan10@gmail.com>
2022-11-05 19:55:34 +0500

    Display the photos
    standalone app project setup
    display the photos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
 })
 export class AppComponent {
   title = 'photo-gallery-app';

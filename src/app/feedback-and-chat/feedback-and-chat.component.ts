83d62d79534c10cf8e7c470feaf8c117be1c6519
asadullahehsan <asadullahehsan10@gmail.com>
2022-11-06 13:57:31 +0500

    Revert "Add event handling to the form"
    This reverts commit 47d37f10feca63596def44683bc8f8895c12b8a8.

--- src/app/feedback-and-chat/feedback-and-chat.component.ts
+++ src/app/feedback-and-chat/feedback-and-chat.component.ts
@@ -1,29 +1,18 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-feedback-and-chat',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule],
   templateUrl: './feedback-and-chat.component.html',
-  styleUrls: ['./feedback-and-chat.component.css'],
+  styleUrls: ['./feedback-and-chat.component.css']
 })
 export class FeedbackAndChatComponent implements OnInit {
-  showMsg: boolean = false;
-  private formBuilder = inject(FormBuilder);
 
-  contactForm = this.formBuilder.group({
-    fullname: '',
-    email: '',
-    comments: '',
-  });
+  constructor() { }
 
-  ngOnInit(): void {}
-
-  onSubmit(): void {
-    console.log('Your feedback has been submitted', this.contactForm.value);
-    this.showMsg = true;
-    this.contactForm.reset();
+  ngOnInit(): void {
   }
+
 }
